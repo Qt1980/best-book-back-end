31517a6485be1801db1e9eb7241f58eead3d0413
Qadree Trimble <qadree_t@yahoo.com>
2021-04-28 21:24:06 -0700

    updated book schema to user schema and added bookschema with properties

--- models/User.js
+++ models/User.js
@@ -1,13 +1,33 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// const activitySchema = new Schema({
+//   activityName: String
+// });
+
+const bookSchema = new Schema({
+  name: String,
+  description: String,
+  status: String,
+  genre: String,
+  isFiction: Boolean,
+  // favoriteActivities data type is an array of activitySchema
+  // favoriteActivities: [activitySchema]
+});
+
 const userSchema = new Schema ({
   name: String,
   email: String,
-  book: [
+  books: [bookSchema
   ]
 });
 
 const User = mongoose.model('User', userSchema);
+
+
+
+// make a model out of the schema
+//  export the model
+
 
 module.exports = User;
